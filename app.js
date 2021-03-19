ecc5f14a0e7515c394c6fcedba38e29f2b0124ac
Bojan-dev <bojanvreco069@gmail.com>
2021-03-19 20:18:58 +0100

    Home sections slider
    Implementation of sections slider in home page

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-let area_container = document.querySelector('.current-area-line');
-let current_area = document.querySelectorAll('.current-area');
-let current_area_title = document.querySelectorAll('.current-area-title');
+'use strict';
 
-current_area.forEach((i) => {
-  i.addEventListener('click', function () {
-    area_container
-      .querySelector('.active-area')
-      .classList.remove('active-area');
+/////////
+//Creating current areas container - Home page:
+/////////
 
-    i.classList.add('active-area');
+const lineThroughtAreas = document.querySelector('.current-area-line');
+
+lineThroughtAreas.style.height = lineThroughtAreas.clientHeight + 50 + 'px'; //setting right slider bar height
+
+const sectionsArray = Array.from(document.querySelectorAll('section'));
+sectionsArray.push(
+  document.querySelector('footer'),
+  document.querySelector('header'),
+  document.querySelector('main')
+);
+
+const sectionArrayFilter = sectionsArray.filter((section) =>
+  section.hasAttribute('sectionName')
+);
+
+const areaRightSliderBtns = Array.from(
+  document.getElementsByClassName('current-area')
+);
+
+areaRightSliderBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const clickedArea = sectionArrayFilter.find(
+      (section) =>
+        section.getAttribute('sectionName') === btn.textContent.trim()
+    );
+    const oldActiveBtn = areaRightSliderBtns.find((sec) =>
+      sec.classList.contains('active-area')
+    );
+    oldActiveBtn.classList.remove('active-area');
+    oldActiveBtn.firstElementChild.classList.remove('active-title');
+    btn.classList.add('active-area');
+    btn.firstElementChild.classList.add('active-title');
+    clickedArea.scrollIntoView();
   });
 });
