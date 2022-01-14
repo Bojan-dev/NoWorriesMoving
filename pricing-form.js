e8c7603d3c9a7aa3d56284c7f74c0bea84c40b0f
Bojan-dev <bojanvreco069@gmail.com>
2022-01-14 18:04:56 +0100

    Pricing calculator almost finished

--- pricing-form.js
+++ pricing-form.js
@@ -8,6 +8,7 @@ const secondFormState = {};
 const firstForm = document.querySelector('.first-calculator-form');
 const secondForm = document.querySelector('.second-calculator-form');
 
+//First form fields:
 const milesFields = document.querySelector('.calculator-miles');
 const homeFields = document.querySelector('.house-apartment');
 const bedroomFields = document.querySelector('.bedrooms-number');
@@ -29,10 +30,34 @@ const lineThrough = document.querySelector('.line-through');
 const firstStepUI = document.querySelector('.calc-steps-1');
 const secondStepUI = document.querySelector('.calc-steps-2');
 
-//Calculator:
+//Second form - Calculator:
 const hourRate = 159;
-const payAllField = document.getElementById('all-in-once');
-const payInstallmentsField = document.getElementById('via-installments');
+// const payAllField = document.getElementById('all-in-once');
+// const payInstallmentsField = document.getElementById('via-installments');
+// const priceFields = document.querySelector('.once-installments');
+const finalPrice = document.getElementById('chosen-final-price');
+
+//Second form fields:
+const nameInput = document.getElementById('lockdown-input-name');
+const emailInput = document.getElementById('lockdown-input-email');
+const phoneInput = document.getElementById('lockdown-input-phone');
+
+//Error fields:
+const nameInputError = document.querySelector('.name-error-message');
+const emailInputError = document.querySelector('.email-error-message');
+const phoneInputError = document.querySelector('.phone-error-message');
+
+//Second form input validation:
+const nameRegex = /^[a-zA-Z\s]*$/;
+const phoneRegex =
+  /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+// prettier-ignore
+const emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+//Successful form:
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal-window');
+const closeModal = document.querySelector('.close-modal');
 
 const controlFormFields = function (type, e) {
   let btn = e.target;
@@ -89,9 +114,11 @@ const controlFormFields = function (type, e) {
     if (btn.classList.contains('house')) {
       threeBedrooms.classList.remove('hidden');
       studio.classList.add('hidden');
+      document.querySelector('.bedroom-number2').textContent = '2 bedrooms';
     } else {
       studio.classList.remove('hidden');
       threeBedrooms.classList.add('hidden');
+      document.querySelector('.bedroom-number2').textContent = '2+ bedrooms';
     }
 
     allBedrooms.forEach((bedroom) => {
@@ -100,11 +127,40 @@ const controlFormFields = function (type, e) {
   }
 };
 
-zip1.addEventListener('focus', function () {
-  zipErrorField.classList.add('hidden');
-  this.style.borderColor = '#a4da89';
-});
+const controlModal = function (toggle) {
+  overlay.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
+};
 
+// const controlPriceFields = function (e) {
+//   let btn = e.target;
+
+//   //prettier-ignore
+//   if(btn.id === '' && !btn.classList.contains('in-once-btn') && !btn.classList.contains('via-installments-btn')) return;
+
+//   let sibling;
+
+//   if (btn.id !== '') btn = btn.parentElement;
+
+//   //prettier-ignore
+//   if (btn.classList.contains('in-once-btn')) sibling = document.querySelector('.via-installments-btn')
+//   //prettier-ignore
+//   if (btn.classList.contains('via-installments-btn')) sibling = document.querySelector('.in-once-btn');
+
+//   sibling.style.background = '#dddede';
+//   btn.style.background = '#41ba03';
+
+//   finalPrice.textContent = btn.querySelector('p').textContent;
+// };
+
+const inputOnFocus = function (errorField) {
+  errorField.classList.add('hidden');
+  this.style.borderColor = '#a4da89';
+};
+
+zip1.addEventListener('focus', inputOnFocus.bind(zip1, zipErrorField));
+
+//First form fields:
 milesFields.addEventListener('click', controlFormFields.bind(this, 'miles'));
 
 homeFields.addEventListener('click', controlFormFields.bind(this, 'home'));
@@ -136,6 +192,7 @@ const showError = function (erorrField, scrollToField, field = false) {
   scrollToField.scrollIntoView({ behavior: 'smooth' });
 };
 
+//On first form submit:
 firstForm.addEventListener('submit', function (e) {
   e.preventDefault();
 
@@ -180,26 +237,105 @@ firstForm.addEventListener('submit', function (e) {
   if (firstFormState.livingObject === 'apartment') {
     firstFormState.bedrooms.toLowerCase() === 'studio' ? (bedroomHrs = 2) : '';
     firstFormState.bedrooms === '1 bedroom' ? (bedroomHrs = 2.5) : '';
-    firstFormState.bedrooms === '2 bedrooms' ? (bedroomHrs = 3) : '';
+    firstFormState.bedrooms === '2+ bedrooms' ? (bedroomHrs = 3) : '';
   }
 
   //CALCULATION:
   const allInOncePrice = (milesHrs + bedroomHrs) * hourRate;
 
-  payAllField.textContent = `$${allInOncePrice}`;
-  payInstallmentsField.textContent = `$${allInOncePrice / 6} X6`;
+  // payAllField.textContent = `$${allInOncePrice}`;
+  // payInstallmentsField.textContent = `$${allInOncePrice / 6} X6`;
+
+  finalPrice.textContent = `$${allInOncePrice}`;
+
+  secondFormState.price = allInOncePrice;
 
   //URL CHANGE:
   activeForm++;
 
-  history.pushState(
-    { id: activeForm },
-    '',
-    `http://127.0.0.1:5501/scss/6-pages/pricing.html#form2`
-  );
+  history.pushState({ id: activeForm }, '', `${location.href}#form2`);
 
   //UI CHANGE:
   changeStepUI('forward');
+});
+
+//Second form fields:
+// priceFields.addEventListener('click', controlPriceFields);
+
+//Inputs on focus:
+nameInput.addEventListener(
+  'focus',
+  inputOnFocus.bind(nameInput, nameInputError)
+);
+emailInput.addEventListener(
+  'focus',
+  inputOnFocus.bind(emailInput, emailInputError)
+);
+phoneInput.addEventListener(
+  'focus',
+  inputOnFocus.bind(phoneInput, phoneInputError)
+);
+
+//Send values to database:
+
+const sendInfo = async function (userData) {
+  try {
+    const response = await fetch(
+      'https://move-calculations-default-rtdb.firebaseio.com/moves.json',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          name: userData.name,
+          email: userData.phoneNumber,
+          phone: userData.phoneNumber,
+          price: userData.price,
+        }),
+      }
+    );
+  } catch (error) {}
+};
+
+//On second form submit:
+secondForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  let controlErrors = 0;
+
+  //prettier-ignore
+
+  if (!phoneRegex.test(phoneInput.value)) {
+    controlErrors++;
+    showError(phoneInputError, phoneInput, phoneInput);
+  }
+
+  if (!emailRegex.test(emailInput.value)) {
+    controlErrors++;
+    showError(emailInputError, emailInput, emailInput);
+  }
+
+  if (
+    !nameRegex.test(nameInput.value) ||
+    nameInput.value.replace(/\s/g, '').length < 2
+  ) {
+    controlErrors++;
+    showError(nameInputError, nameInput, nameInput);
+  }
+
+  if (controlErrors) return;
+
+  secondFormState.name = nameInput.value.trim();
+  secondFormState.phoneNumber = phoneInput.value;
+  secondFormState.emailAddress = emailInput.value;
+
+  sendInfo(secondFormState);
+
+  controlModal('show');
+});
+
+closeModal.addEventListener('click', () => {
+  activeForm = 1;
+  controlModal('close');
+  document.location.reload();
 });
 
 window.addEventListener('hashchange', function () {
@@ -213,6 +349,6 @@ window.addEventListener('load', function () {
   history.pushState(
     { id: activeForm },
     '',
-    'http://127.0.0.1:5501/scss/6-pages/pricing.html'
+    `${location.origin}/scss/6-pages/pricing.html`
   );
 });
